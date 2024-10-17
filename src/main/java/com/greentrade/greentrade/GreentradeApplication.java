69a38266fab3507e25a284740dc463283a08b07a
damienverkerk <damien-verkerk@hotmail.com>
2024-10-17 18:06:17 +0200

    Opmaak project

---
+++ src/main/java/com/greentrade/greentrade/GreentradeApplication.java
@@ -0,0 +1,13 @@
+package com.greentrade.greentrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreentradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreentradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greentrade/greentrade/GreentradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greentrade.greentrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreentradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

98fbac3b1c054de80f1b43594fd3e830c4d7e87c
damienverkerk <damien-verkerk@hotmail.com>
2024-10-24 01:48:24 +0200

    Verbeter Swagger configuratie en voeg Swagger UI toegang toe

--- src/main/java/com/greentrade/greentrade/config/SwaggerConfig.java
+++ src/main/java/com/greentrade/greentrade/config/SwaggerConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -18,9 +20,18 @@ public class SwaggerConfig {
     public OpenAPI greenTradeOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("GreenTrade API")
-                        .description("API voor de GreenTrade applicatie - Een platform voor handel in duurzame producten")
-                        .version("v0.0.1")
+                        .title("GreenTrade API Documentatie")
+                        .description("""
+                            Documentatie voor de GreenTrade REST API - Een platform voor handel in duurzame producten.
+                            
+                            Deze API biedt functionaliteiten voor:
+                            * Beheer van duurzame producten
+                            * Transactiebeheer
+                            * Certificaatvalidatie
+                            * Gebruikerscommunicatie
+                            * Beoordelingssysteem
+                            """)
+                        .version("1.0.0")
                         .contact(new Contact()
                                 .name("GreenTrade Support")
                                 .email("support@greentrade.nl")
@@ -31,7 +42,13 @@ public class SwaggerConfig {
                 .servers(List.of(
                         new Server()
                                 .url("http://localhost:8080")
-                                .description("Development server")
-                ));
+                                .description("Development Server")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }

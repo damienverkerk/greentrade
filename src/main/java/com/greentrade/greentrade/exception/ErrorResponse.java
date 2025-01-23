1f9661372aa6a8568412b8626680a78eec48bd34
damienverkerk <damien-verkerk@hotmail.com>
2025-01-23 01:05:09 +0100

    Voeg error modellen toe, Error response klasse en ValidationErrorResponse voor validatie fouten.

---
+++ src/main/java/com/greentrade/greentrade/exception/ErrorResponse.java
@@ -0,0 +1,42 @@
+// Path: src/main/java/com/greentrade/greentrade/exception/ErrorResponse.java
+
+package com.greentrade.greentrade.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String message, LocalDateTime timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    // Getters en setters
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
